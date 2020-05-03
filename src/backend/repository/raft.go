98c2d98c80885df24bf3bd819a3d2d7535792bb1
umar <swumar@gmail.com>
2020-05-03 10:55:18 +0530

    Adding put,get

---
+++ src/backend/repository/raft.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"context"
+	"go.etcd.io/etcd/clientv3"
+	"log"
+	"time"
+)
+
+func Put(key string,value string,ctx context.Context) error{
+	cli, err := clientv3.New(clientv3.Config{
+		DialTimeout: 5 * time.Second,
+		Endpoints: []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+	_, puterr := cli.Put(ctx, key, value)
+	if puterr != nil {
+		log.Fatal(err)
+		return puterr
+	}else {
+		return nil
+	}
+
+}
+
+func Get(key string,ctx context.Context) ([]byte,error){
+	cli, err := clientv3.New(clientv3.Config{
+		DialTimeout: 5 * time.Second,
+		Endpoints: []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+
+	response, geterr := cli.Get(ctx, key)
+	if geterr != nil {
+		log.Fatal(err)
+		return []byte(""),geterr
+	}else {
+		if len(response.Kvs) == 0{
+			return []byte(""),nil
+		}else{
+			return response.Kvs[0].Value,nil
+		}
+
+	}
+
+}
