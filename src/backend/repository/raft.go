5bb0412700b5cb575b14d3471e6b2ab6074984b7
umar <swumar@gmail.com>
2020-05-04 12:22:19 +0530

    adding delete

--- src/backend/repository/raft.go
+++ src/backend/repository/raft.go
@@ -74,3 +74,20 @@ func GetAll(ctx context.Context) (string,error){
 	}
 
 }
+
+func Delete(ctx context.Context){
+	cli, err := clientv3.New(clientv3.Config{
+		DialTimeout: 5 * time.Second,
+		Endpoints: []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+	opts := []clientv3.OpOption{
+		clientv3.WithPrefix(),
+	}
+
+	cli.Delete(ctx,"",opts...)
+
+}
