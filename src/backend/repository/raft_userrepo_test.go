14069084927e827fb5ca3a5246cb6dd29625c97c
umar <swumar@gmail.com>
2020-05-04 13:07:16 +0530

    Adding testcases

--- src/backend/repository/raft_userrepo_test.go
+++ src/backend/repository/raft_userrepo_test.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 	"sync"
 	"testing"
-	//"time"
+	"time"
 )
+
+//Adding 10 users in TestSaveUser which will be used as mockup data for other test cases. So please run the test cases
+//in the same order if running it individually.
 
 func TestSaveUser(t *testing.T) {
 	Delete(context.Background())
@@ -36,9 +39,109 @@ func TestSaveUser(t *testing.T) {
 	t.Log(res)
 	resSlice := strings.Split(res,",")
 	if len(resSlice) == 10{
-		t.Log("Test SaveUser succesful")
+		t.Log("Test SaveUser successful")
 	}else {
-		t.Error("Test case unsucessful")
+		t.Error("Test SaveUser unsuccessful")
+	}
+
+}
+
+func TestReturnUser(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		username := "test1_0@gmail.com" //added in TestSaveUser test case
+		user, _, err := ReturnUser(username,context.Background())
+		if err != nil {
+			t.Error("Test ReturnUser unsuccessful")
+		}else {
+			t.Log(user.Username)
+			t.Log("Test ReturnUser successful")
+		}
+	}()
+	wg.Wait()
+}
+
+func TestReturnUserContext(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		username := "test1_0@gmail.com" //added in TestSaveUser test case
+		ctx , cancel := context.WithTimeout(context.Background(),time.Duration(100)*time.Second)
+		cancel()
+		user, _, err := ReturnUser(username,ctx)
+		if err != nil {
+			t.Log("Test ReturnUserContext successful")
+		}else {
+			t.Log(user.Username)
+			t.Error("Test ReturnUserContext unsuccessful")
+		}
+	}()
+	wg.Wait()
+}
+
+func TestGetUsers(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		users, err := GetUsers(context.Background())
+		if err != nil {
+			t.Error("Test GetUsers unsuccessful")
+		}else {
+			t.Log(users)
+			t.Log("Test GetUsers successful")
+		}
+	}()
+	wg.Wait()
+}
+
+func TestGetUsersContext(t *testing.T) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ctx , cancel := context.WithTimeout(context.Background(),time.Duration(100)*time.Second)
+		cancel()
+		users, err := GetUsers(ctx)
+		if err != nil {
+			t.Log("Test GetUsersContext successful")
+		}else {
+			t.Log(users)
+			t.Error("Test GetUsersContext unsuccessful")
+		}
+	}()
+	wg.Wait()
+}
+
+func TestSaveUserContext(t *testing.T) {
+	Delete(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		user := model.User{
+			Username:  "test2_1@gmail.com",
+			Password:  "1234",
+			FirstName: "us1",
+			LastName:  "er1",
+		}
+		ctx , cancel := context.WithTimeout(context.Background(),time.Duration(100)*time.Second)
+		cancel()
+		err := SaveUser(user,ctx)
+		if err == nil {
+			t.Error("User added succesfully")
+		}
+	}()
+	wg.Wait()
+	res,_ := GetAll(context.Background())
+	if len(res) == 0{
+		t.Log("Test SaveUserContext succesful")
+	}else {
+		t.Log(res)
+		t.Error("Test SaveUserContext unsucessful")
 	}
 
 }
